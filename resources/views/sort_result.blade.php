1cc1fac876b4987ce193c98b5351f5be973475e8
Hundiak Valeriia <114095623+pojuipolynu@users.noreply.github.com>
2023-05-24 22:23:10 +0300

    Delete sort_result.blade.php

--- resources/views/sort_result.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('template.recipetemplate')
-@section('content')
-<p style="color:#535E4B;">Людина коментувала такі страви:</p>
-@if($check)
-    @foreach($check->all_dish as $x) 
-            <p style="color:#535E4B;"> {{$x->dish_name}}</p>
-    @endforeach
-@endif
-@endsection
